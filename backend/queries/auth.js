7b13976e71218fa9dd8db552d51197da07d66171
Emmanuelkorede <emmanueljob2009@gmail.com>
2026-06-26 14:49:51 +0100

    feat : built the  create user query and created the routes page for it

--- backend/queries/auth.js
+++ backend/queries/auth.js
@@ -1,3 +1,21 @@
 const pool = require('../db') ;
 const handleDbError = require('../middleware/dberror')
 const bcrypt = require('bcrypt') ; 
+const jwt = require('jsonwebtoken') ; 
+
+const getUserByemail = async (email) => {
+    const result = await pool.query('SELECT * FROM users WHERE email = $1' , [email]) ; 
+    return result.rows ; 
+}
+
+const createUser = (req , res) => {
+    try {
+        const {name , email , password } = req.body ; 
+        const hashedPassword  = await bcrypt.hash(password ,10) ; 
+        const result = await pool.query('INSERT INTO users (name , email , password) VALUES ($1 , $2 , $3) RETURNING *'[name , email , hashedPassword]) ;
+        res.status(201).json({message : 'user created successfully' , result : result.rows})
+    }catch(err) {
+        handleDbError(err , res)
+    }
+
+}
---
+++ backend/routes/auth.js
@@ -0,0 +1,4 @@
+const express = require('express') ;
+const router = express.Router() ;
+const validate = require('../middleware/validate') 
+
